b91d064366c4f63c173bd9582307649ed169c82f
Jesse Talavera-Greenberg <jesse@jesse.tg>
2021-10-14 17:32:37 -0400

    Make FinallyScope public
    - Storing it as an IDisposable boxes it

---
+++ src/QuikGraph/Helpers/DisposableHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace QuikGraph.Utils
+{
+    /// <summary>
+    /// Helpers to work with <see cref="IDisposable"/>.
+    /// </summary>
+    public static class DisposableHelpers
+    {
+        /// <summary>
+        /// Calls an action when going out of scope.
+        /// </summary>
+        /// <param name="action">The action to call.</param>
+        /// <returns>A <see cref="IDisposable"/> object to give to a using clause.</returns>
+        [Pure]
+        public static FinallyScope Finally([NotNull] Action action)
+        {
+            return new FinallyScope(action);
+        }
+    }
+}
---
+++ src/QuikGraph/Helpers/FinallyScope.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace QuikGraph.Utils
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public struct FinallyScope : IDisposable
+    {
+        private Action _action;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="action">The action to call upon disposal of this scope.</param>
+        public FinallyScope([NotNull] Action action)
+        {
+            Debug.Assert(action != null);
+
+            _action = action;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _action?.Invoke();
+            _action = null;
+        }
+    }
+}
